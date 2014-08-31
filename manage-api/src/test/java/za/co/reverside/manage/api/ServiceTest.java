6cdcdc66945325b51924497d0b467c59a8d4e84a
M2 <manmay.e.mohanty@gmail.com>
2014-08-31 19:06:44 +0200

    Adding my first test case

---
+++ manage-api/src/test/java/za/co/reverside/manage/api/ServiceTest.java
@@ -0,0 +1,16 @@
+package za.co.reverside.manage.api;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class ServiceTest {
+
+
+    @Test
+    public void testGetTime(){
+        Service service = new Service();
+        String result = service.getTime();
+        assertNotNull(result);
+    }
+}
