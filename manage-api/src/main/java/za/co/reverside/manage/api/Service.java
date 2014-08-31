c70c27d39556231eceb1f9011892580d36d4c1eb
M2 <manmay.e.mohanty@gmail.com>
2014-08-31 18:10:03 +0200

    Adding a new service to get employee details

--- manage-api/src/main/java/za/co/reverside/manage/api/Service.java
+++ manage-api/src/main/java/za/co/reverside/manage/api/Service.java
@@ -1,20 +1,31 @@
 package za.co.reverside.manage.api;
 
 
-import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import za.co.reverside.manage.model.Employee;
 
 import java.util.Date;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 public class Service {
 
-
-    @RequestMapping(value = "/api/time", method = RequestMethod.GET)
+    @RequestMapping(value = "api/time", method = GET)
     public String getTime(){
         return new Date().toString();
+    }
+
+    @RequestMapping(value = "api/employee/{id}", method = GET)
+    public Employee getEmployee(@PathVariable("id") Long id){
+        Employee employee = new Employee();
+        employee.setId(id);
+        employee.setFirstName("Manmay");
+        employee.setLastName("Mohanty");
+        employee.setEmail("manmay.mohanty@reverside.co.za");
+        return employee;
     }
 
 }
---
+++ manage-api/src/main/java/za/co/reverside/manage/model/Employee.java
@@ -0,0 +1,11 @@
+package za.co.reverside.manage.model;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+    private Long id;
+    private String firstName ;
+    private String lastName;
+    private String email;
+}
