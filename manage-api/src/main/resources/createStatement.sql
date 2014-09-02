f5bf80accc0da693279fedb0a4d927ad47a7b7fe
M2 <manmay.e.mohanty@gmail.com>
2014-09-02 20:28:31 +0200

    Adding TODO

--- manage-api/src/main/resources/createStatement.sql
+++ manage-api/src/main/resources/createStatement.sql
@@ -2,15 +2,17 @@ delimiter $$
 
 CREATE TABLE `userdetail` (
   `id` int(11) NOT NULL,
-  `name` varchar(45) NOT NULL,
-  `email` varchar(45) NOT NULL,
-  `phonenumber` int(11) DEFAULT NULL,
-  `imgurl` varchar(45) DEFAULT NULL,
+  'employee_id' varchar(45) NOT NULL,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
   `designation` varchar(45) DEFAULT NULL,
-  `urls` varchar(45) DEFAULT NULL,
+  `email` varchar(45) NOT NULL,
+  `phone` varchar(45) DEFAULT NULL,
+  `image` varchar(45) DEFAULT NULL,
+  'message' varchar(1000) DEFAULT NULL
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`),
-  UNIQUE KEY `phonenumber_UNIQUE` (`phonenumber`)
+  UNIQUE KEY `phone_UNIQUE` (`phone`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
 
