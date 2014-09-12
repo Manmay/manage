a49923c344e9cfedffba8443e211cc1440dc3432
Manmay <manmay.e.mohanty@gmail.com>
2014-09-12 19:47:22 +0200

    Design list page

--- manage-web/src/main/webapp/js/app.js
+++ manage-web/src/main/webapp/js/app.js
@@ -4,11 +4,11 @@ var app = angular.module('app', ['ngRoute']);
 app.config(['$routeProvider',
     function ($routeProvider) {
         $routeProvider
-            .when('/employee', {
+            .when('/employee/:mode', {
                 'templateUrl': 'html/employee.html',
                 'controller': 'EmployeeController'
             })
-            .when('/employee/:id', {
+            .when('/employee/:id/:mode', {
                 'templateUrl': 'html/employee.html',
                 'controller': 'EmployeeController'
             });
@@ -25,11 +25,15 @@ app.directive("ngFileSelect", function () {
     }
 });
 
-app.controller('EmployeeController', function ($scope, $routeParams) {
+app.controller('EmployeeController', function ($scope, $routeParams, $location) {
 
     $scope.mode ;
 
     $scope.employee ;
+
+    $scope.employees =[] ;
+
+    $scope.searchText;
 
     $scope.init = function(){
         console.log($routeParams.mode);
@@ -39,7 +43,52 @@ app.controller('EmployeeController', function ($scope, $routeParams) {
                 "score": "0000",
                 "photo": "img/new-user.jpg"
             };
-        } else {
+        }else if($scope.mode == 'search'){
+            $scope.employees = [
+                {
+                    "id" : 1,
+                    "fullName": "Manmay Mohanty",
+                    "designation": "Senior Consultant",
+                    "email": "manmay.mohanty@reverside.co.za",
+                    "phone": "+27 749210674",
+                    "quote": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac risus nibh.",
+                    "score": "0025",
+                    "photo": "img/manmay.jpg",
+                    "address" : {
+                        "city" : "Johannesberg",
+                        "country" : "South Africa"
+                    }
+                },
+                {
+                    "id" : 2,
+                    "fullName": "Soumya Pattanaik",
+                    "designation": "Associate Consultant",
+                    "email": "soumya.pattanaik@reverside.co.za",
+                    "phone": "+27 749210674",
+                    "quote": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac risus nibh.",
+                    "score": "0025",
+                    "photo": "img/soumya.jpg",
+                    "address" : {
+                        "city" : "Pretoria",
+                        "country" : "South Africa"
+                    }
+                },
+                {
+                    "id" : 3,
+                    "fullName": "Pragati Prusty",
+                    "designation": "Associate Consultant",
+                    "email": "pragati.prusty@reverside.co.za",
+                    "phone": "+27 749210674",
+                    "quote": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac risus nibh.",
+                    "score": "0025",
+                    "photo": "img/pragati.jpg",
+                    "address" : {
+                        "city" : "Sandton",
+                        "country" : "South Africa"
+                    }
+                }
+            ];
+        }else {
             $scope.employee = {
                 "fullName": "Soumya Pattanaik",
                 "designation": "Associate Consultant",
@@ -47,10 +96,14 @@ app.controller('EmployeeController', function ($scope, $routeParams) {
                 "phone": "+27 749210674",
                 "quote": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac risus nibh.",
                 "score": "0025",
-                "photo": "user_avatar.jpg"
+                "photo": "img/soumya.jpg"
             };
         }
 
+    };
+
+    $scope.showEmployee = function(id){
+      $location.path(decodeURI('/employee/'+id+'/view'));
     };
 
     $scope.hello = function (file) {
